e458ed57495b20c42c31f6684905d4907451e1b7
wala41447oueslati <wala41447@gmail.com>
2026-08-27 11:42:12 +0100

    Delete node_modules directory

--- node_modules/pdfkit/tools/convert-afm-parsed.js
+++
@@ -1,8 +0,0 @@
-const { convertFiles } = require('./afm-converter');
-
-try {
-  convertFiles(process.argv.slice(2), 'parsed');
-} catch (error) {
-  console.error(error.message);
-  process.exitCode = 1;
-}
