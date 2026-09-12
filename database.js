7051917f4f98978cf76d7a42a9333a2ab26d1571
wala41447oueslati <wala41447@gmail.com>
2026-09-12 16:04:49 +0100

    fix  database

--- database.js
+++ database.js
@@ -1,20 +1,27 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST || "localhost",
-  port: process.env.MYSQLPORT || 3306,
-  user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "",
-  database: process.env.MYSQLDATABASE || "tunisian_taekwondo_federation"
+const db = mysql.createPool({
+    host: process.env.MYSQLHOST || "localhost",
+    port: process.env.MYSQLPORT || 3306,
+    user: process.env.MYSQLUSER || "root",
+    password: process.env.MYSQLPASSWORD || "",
+    database: process.env.MYSQLDATABASE || "tunisian_taekwondo_federation",
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error("Erreur MySQL :", err);
-    return;
-  }
+// Tester la connexion
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error("Erreur MySQL :", err);
+        return;
+    }
 
-  console.log("MySQL connecté !");
+    console.log("MySQL connecté !");
+
+    connection.release();
 });
 
 module.exports = db;
